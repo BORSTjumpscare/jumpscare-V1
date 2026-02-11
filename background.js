d05d3382c2487a51a6d0ce675d751512e73bc350
BORSTjumpscare <sepperem@gmail.com>
2026-02-11 22:57:11 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,24 +1,19 @@
+// Track currently active/focused tab
 let activeTabId = null;
 
-// Track tab focus
 chrome.tabs.onActivated.addListener(activeInfo => {
     activeTabId = activeInfo.tabId;
 });
 
 chrome.windows.onFocusChanged.addListener(windowId => {
-    if (windowId === chrome.windows.WINDOW_ID_NONE) {
-        activeTabId = null; // lost focus
-    } else {
-        chrome.tabs.query({ active: true, windowId: windowId }, tabs => {
-            if (tabs.length) activeTabId = tabs[0].id;
-        });
-    }
+    chrome.tabs.query({ active: true, windowId: windowId }, tabs => {
+        if (tabs.length) activeTabId = tabs[0].id;
+    });
 });
 
 // Respond to content script focus check
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.action === "checkFocus") {
-        const isFocused = sender.tab && sender.tab.id === activeTabId;
-        sendResponse({ isFocused: !!isFocused });
+        sendResponse({ isFocused: sender.tab?.id === activeTabId });
     }
 });
